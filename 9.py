bdf0379d4f6aa39dd1eb2ed803f159d625a94d48
los16dura <Ulechka_1999@mail.ru>
2016-09-16 23:57:25 +0300

    Add files via upload
    Версия 2,нормальная вроде

--- 9.py
+++ 9.py
@@ -7,8 +7,9 @@ turtle.shape('turtle')
 
 def ris(n):
     t = n
+    turtle.left(90+180/t)
     while n > 0:
-        turtle.forward(t*10+10)
+        turtle.forward(t*11+10)
         turtle.left(360/t)
         n -= 1
 
@@ -16,16 +17,17 @@ def ris(n):
 i = 3
 l = 10
 p = 1
+turtle.penup()
+turtle.goto(10,0)
+turtle.pendown()
 while i <= k:
     ris(i)
     i +=1
+    turtle.right(90+180/(i-1))
     turtle.penup()
-    turtle.goto(-5*p,0)
-    turtle.right(90)
-    turtle.forward(l)
-    turtle.left(90)
+    turtle.goto(0,0)
+    turtle.forward(20*p)
     turtle.pendown()
-    l=l+10
     p+=1
 
 #turtle.forward(50)
